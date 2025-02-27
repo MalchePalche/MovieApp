afa92e1612ae3b61da46ad74c4e34425fa77c861
Maksim_Palcheff <maxim@palcheff.com>
2025-02-27 20:06:00 +0200

    Initial project setup with .gitignore

---
+++ IMDBClone/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IMDBClone.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
